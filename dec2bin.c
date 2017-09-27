e0812677022f9b3b32a9fa495d6402f259158dd5
vnigolian <valentin.nigolian@epfl.ch>
2017-09-27 14:38:41 +0200

    mostly done dec2bin but stuck with annoying char stuff

--- dec2bin.c
+++ dec2bin.c
@@ -2,56 +2,64 @@
  *
  * Decimal to binary and reverse conversions.
  *
- * Version: 1.0 (170901)
- * Author: J.-C. Chappelier (EPFL, 2017)
+ * Version: 1.0
+ * Author: VZ Nigolian (EPFL, 2017)
  *
  * ======================================================================
  */
-#include <iostream>
-#include <string>
-using namespace std;
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+#define MAX_LENGTH 256
 
 /* ======================================================================
  * This is the first algorithm of interest:
  * convertion from positive decimal to binary.
  */
-string binary_nonnegative(int n)
+void binary_nonnegative(int n, char* output)
 {
   if (n <= 0) {
-    return "0";
+    strcpy(output, "0");
+    return;
   }
 
   // here n >= 1
-  string output;
   while (n > 0) {
-    output = char('0' + n%2) + output;
-    /* We could also make use of a less compact form, like:
-       if (n%2 == 1) {
-           output = '1' + output;
-       } else {
-           output = '0' + output;
-       }
-    */
-    
-    n /= 2;
+	  if (n%2 == 1) {
+		  char temp[] ="1";
+		  strcpy(output,strcat(temp,output));
+      }else{
+		  char temp[] = "0";
+		  strcpy(output,strcat(temp,output));
+      }
+     
+      n /= 2;
   }
   
-  return output;
 }
 
 /* ======================================================================
  * This is the second algorithm of interest:
  * convertion from positive binary (no sign bit) to decimal.
  */
-unsigned int decimal_nonnegative(string const& binary)
-// Note: from C++-17, make use of string_view rather than "string const&"
+unsigned int decimal_nonnegative(char* binary)
 {
-  unsigned int output(0);
-  unsigned int power(1);
+  unsigned int output=0;
+  unsigned int power=1;
 
   /* There are more advanced way to iterate over a const string in C++ (crbegin(),
    * crend()). This is the very basic way for beginners in C++ programming.
    */
+   if(strlen(binary)>0){
+	   for(int i=strlen(binary)-1;i<strlen(binary);--i){
+		   if(binary[i]=='1'){
+			   output+= power;
+		   }
+		   power*=2;
+	   }   
+   }
+   /*
   if (not binary.empty()) {
     for (size_t i(binary.size() - 1); i < binary.size(); --i) {
       if (binary[i] == '1') {
@@ -59,7 +67,7 @@ unsigned int decimal_nonnegative(string const& binary)
       }
       power *= 2;
     }
-  }
+  }*/
 
   return output;
 }
@@ -68,65 +76,64 @@ unsigned int decimal_nonnegative(string const& binary)
  * This is the third algorithm of interest:
  * two's complement of binary string
  */
-string twos_complement(string const& binary)
-// Note: from C++-17, make use of string_view rather than "string const&"
+void twos_complement(const char* binary,char* output)
 {
-  string output(binary); // starts from a copy
-
-  if (not binary.empty()) {
+  strcpy(output,binary);
+  if (strlen(binary)>0) {
     
     // invert all bits left to the least-signicant 1 (LS1)
-
-    // first search for LS1
-    /* There are more advanced way to iterate over a string in C++ (cbegin(),
-     * cend()). This is the very basic way for beginners in C++ programming.
-     */
-    size_t i(output.size() - 1);
-    while ((i < output.size()) and (output[i] != '1')) --i;
-
-    // skip that LS1
-    --i;
-
-    // then invert all "higher" bits
-    while (i < output.size()) {
-      if (output[i] == '1') {
-        output[i] = '0';
-      } else {
-        output[i] = '1';
-      }
-      --i;
+    int i=strlen(output)-1;
+    while(i<strlen(output) && (output[i] != '1')) {
+		--i;
     }
-  }
-  
-  return output;
+    --i;
+    
+    while(i<strlen(output)){
+		if(output[i] == '1'){
+			output[i] = '0';
+		}else{
+			output[i] = '1';
+		}
+		--i;
+	}
+  }  
 }
 
 /* ======================================================================
  * This is the forth algorithm of interest:
  * convertion from (signed) decimal to binary, making use of former algorithms.
  */
-string binary(int n)
+void binary(int n, char* output)
 {
   // Add the bit sign to the corresponding "unsigned" representation
   if (n < 0) {
-    return "1" + twos_complement(binary_nonnegative(-n));
-    /* could also be written as:
-     * return twos_complement("0"s + binary_nonnegative(-n));
-     */
+	  char temp[] = "1";
+      char nonnegative[MAX_LENGTH]="";
+	  binary_nonnegative(-n,nonnegative);
+	  twos_complement(nonnegative,output);
+      strcat(temp,output );
+      strcpy(output,temp);
+      return;
+
   }
-  return "0" + binary_nonnegative(n);
+  char temp[] = "0";
+  char nonnegative[MAX_LENGTH]="";
+  binary_nonnegative(n,nonnegative);
+  strcat(temp, nonnegative);
+  strcpy(output,temp);
 }
 
 /* ======================================================================
  * This is the fifth algorithm of interest:
  * convertion from binary (with sign bit) to decimal.
  */
-int decimal(string const& binary)
-// Note: from C++-17, make use of string_view rather than "string const&"
+int decimal(char* binary)
 {
   // test sign bit
-  if (not binary.empty() and (binary[0] == '1')) {
-    return -decimal_nonnegative(twos_complement(binary));
+  if (strlen(binary)>0 && (binary[0] == '1')) {
+	  char* complement="";
+	  twos_complement(binary,complement);
+     return -decimal_nonnegative(complement);
   }
 
   return decimal_nonnegative(binary);
@@ -142,9 +149,11 @@ int decimal(string const& binary)
  */
 void test(int n)
 {
-  const string result(binary(n));
-  cout << n << " is " << result << endl;
-  cout << "and " << result << " is indeed " << decimal(result) << '.' << endl;
+  char result[]="";
+  binary(n,result);
+  printf("%i is %s\n",n,result);
+  printf("and %s is indeed %i.\n",result,decimal(result));
+  printf("done testing\n");
 }
 
 /* ======================================================================
@@ -152,36 +161,46 @@ void test(int n)
  */
 int require_int()
 {
-  int value(0);
-  cout << "Enter some integer: ";
-  cin >> value;
+  int value=0;
+  printf("Enter some integer\n");
+  scanf("%i",&value);
+  
   return value;
 }
 
 /* ======================================================================
  * Tool function: ask to continue or not.
+ * 0 = no, >0 = yes
  */
-bool go_ahead() {
-  char read('x');
+int go_ahead() {
+  char read='x';
   do {
-    cout << "Shall we continue (y/n)? ";
-    cin >> read;
-  } while ((read != 'y') and (read != 'Y') and
-           (read != 'n') and (read != 'N'));
-  return (read == 'y') or (read == 'Y');
+	  printf("Shall we continue (y/n)?\n");
+      scanf("%c",&read);
+  } while ((read != 'y') && (read != 'Y') &&
+           (read != 'n') && (read != 'N'));
+  return (read == 'y') || (read == 'Y');
 }
 
 // ======================================================================
 int main()
 {
-  const int t(42);
-  cout << binary_nonnegative(t) << endl;
+  const int t=42;
+  char bin[MAX_LENGTH]="";
+  binary_nonnegative(t,bin);
+  printf("conversion : %s\n",bin);
+  
   test(t);
-  test(-t);
+  
+  printf("done first test\n");
+  /*test(-t);
+  
+  printf("done second test\n");
   
   do {
     test(require_int());
   } while (go_ahead());
 
   return 0;
+  * */
 }
