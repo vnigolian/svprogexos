ab27390727dc71c6ebd231d9c2d6cb1042759a1c
Nigolian Valentin Zenon <nigolian@IC-CO-IN-SC215.intranet.epfl.ch>
2017-10-13 11:16:25 +0200

    done dec2bin with arrays

--- dec2bin.c
+++ dec2bin.c
@@ -10,14 +10,24 @@
 #include "stdio.h"
 #include "string.h"
 #include "stdlib.h"
+#include "math.h"
 
-#define MAX_LENGTH 256
+
+void printTab(int size, char tab[size]){
+	for(int i=0;i<size;i++){
+		printf("%c",tab[i]);
+	}
+	printf("\n");
+	
+}
+
 
 /* ======================================================================
  * This is the first algorithm of interest:
  * convertion from positive decimal to binary.
  */
-void binary_nonnegative(int n, char* output)
+
+void binary_nonnegative(int n, int size, char output[size])
 {
   if (n <= 0) {
     strcpy(output, "0");
@@ -25,16 +35,16 @@ void binary_nonnegative(int n, char* output)
   }
 
   // here n >= 1
-  while (n > 0) {
+  int i=size-1;
+  while (n > 0 && i<size ) {
 	  if (n%2 == 1) {
-		  char temp[] ="1";
-		  strcpy(output,strcat(temp,output));
+		  output[i]='1';
       }else{
-		  char temp[] = "0";
-		  strcpy(output,strcat(temp,output));
+		  output[i]='0';
       }
      
       n /= 2;
+      i--;
   }
   
 }
@@ -43,7 +53,7 @@ void binary_nonnegative(int n, char* output)
  * This is the second algorithm of interest:
  * convertion from positive binary (no sign bit) to decimal.
  */
-unsigned int decimal_nonnegative(char* binary)
+unsigned int decimal_nonnegative(int size, char binary[size])
 {
   unsigned int output=0;
   unsigned int power=1;
@@ -51,23 +61,14 @@ unsigned int decimal_nonnegative(char* binary)
   /* There are more advanced way to iterate over a const string in C++ (crbegin(),
    * crend()). This is the very basic way for beginners in C++ programming.
    */
-   if(strlen(binary)>0){
-	   for(int i=strlen(binary)-1;i<strlen(binary);--i){
+   if(size>0){
+	   for(int i=strlen(binary)-1;i>0;--i){
 		   if(binary[i]=='1'){
 			   output+= power;
 		   }
 		   power*=2;
 	   }   
    }
-   /*
-  if (not binary.empty()) {
-    for (size_t i(binary.size() - 1); i < binary.size(); --i) {
-      if (binary[i] == '1') {
-        output += power;
-      }
-      power *= 2;
-    }
-  }*/
 
   return output;
 }
@@ -76,25 +77,24 @@ unsigned int decimal_nonnegative(char* binary)
  * This is the third algorithm of interest:
  * two's complement of binary string
  */
-void twos_complement(const char* binary,char* output)
+void twos_complement(int size, const char binary[size], char output[size])
 {
   strcpy(output,binary);
-  if (strlen(binary)>0) {
+  if (size>0) {
     
     // invert all bits left to the least-signicant 1 (LS1)
-    int i=strlen(output)-1;
-    while(i<strlen(output) && (output[i] != '1')) {
-		--i;
+    int i=size-1;
+    while(i>1 && (output[i] != '1')) {
+		i--;
     }
-    --i;
-    
-    while(i<strlen(output)){
+
+    while(i>0){
+		i--;
 		if(output[i] == '1'){
 			output[i] = '0';
 		}else{
 			output[i] = '1';
 		}
-		--i;
 	}
   }  
 }
@@ -103,40 +103,41 @@ void twos_complement(const char* binary,char* output)
  * This is the forth algorithm of interest:
  * convertion from (signed) decimal to binary, making use of former algorithms.
  */
-void binary(int n, char* output)
+void binary(int n, int size, char output[size])
 {
   // Add the bit sign to the corresponding "unsigned" representation
   if (n < 0) {
 	  char temp[] = "1";
-      char nonnegative[MAX_LENGTH]="";
-	  binary_nonnegative(-n,nonnegative);
-	  twos_complement(nonnegative,output);
+      char nonnegative[size-1];
+	  binary_nonnegative(-n,size-1,nonnegative);
+	  twos_complement(size-1,nonnegative,output);
       strcat(temp,output );
       strcpy(output,temp);
       return;
 
+  }else{
+	char temp[] = "0";
+	char nonnegative[size-1];
+	binary_nonnegative(n,size-1,nonnegative);
+	strcat(temp, nonnegative);
+	strcpy(output,temp);
   }
-  char temp[] = "0";
-  char nonnegative[MAX_LENGTH]="";
-  binary_nonnegative(n,nonnegative);
-  strcat(temp, nonnegative);
-  strcpy(output,temp);
 }
 
 /* ======================================================================
  * This is the fifth algorithm of interest:
  * convertion from binary (with sign bit) to decimal.
  */
-int decimal(char* binary)
+int decimal(int size, char binary[size])
 {
   // test sign bit
-  if (strlen(binary)>0 && (binary[0] == '1')) {
-	  char* complement="";
-	  twos_complement(binary,complement);
-     return -decimal_nonnegative(complement);
+  if (size>0 && (binary[0] == '1')) {
+	  char complement[size];
+	  twos_complement(size,binary,complement);
+     return -decimal_nonnegative(size,complement);
   }
 
-  return decimal_nonnegative(binary);
+  return decimal_nonnegative(size,binary);
 }
 
 /* ======================================================================
@@ -149,10 +150,16 @@ int decimal(char* binary)
  */
 void test(int n)
 {
-  char result[]="";
-  binary(n,result);
+  int size = ceil(log2(abs(n)))+1;
+  char result[size];
+  
+  for(int i=0;i<size;i++){
+	  result[i]='0';
+  }
+  
+  binary(n,size,result);
   printf("%i is %s\n",n,result);
-  printf("and %s is indeed %i.\n",result,decimal(result));
+  printf("and %s is indeed %i.\n",result,decimal(size,result));
   printf("done testing\n");
 }
 
@@ -185,22 +192,10 @@ int go_ahead() {
 // ======================================================================
 int main()
 {
-  const int t=42;
-  char bin[MAX_LENGTH]="";
-  binary_nonnegative(t,bin);
-  printf("conversion : %s\n",bin);
-  
-  test(t);
-  
-  printf("done first test\n");
-  /*test(-t);
-  
-  printf("done second test\n");
-  
-  do {
-    test(require_int());
-  } while (go_ahead());
-
+  test(42);
+  test(-42);
+  test(12314);
+  test(-(pow(2,5)-1));
   return 0;
-  * */
+  
 }
