6cf09d942c89e0374ee1aa46ec74423ec9dc27b8
vnigolian <valentin.nigolian@epfl.ch>
2017-12-12 12:18:24 +0100

    first draft of exercise

--- nyquist.c
+++ nyquist.c
@@ -88,22 +88,23 @@ int main()
  
   double t_min=0.0;
   double t_max=2.0;
-  
-  struct Echantillonage ech1;
   double freq1 = 20.0;
+  double freq2 = 11.0;
+  
+  struct Echantillonage ech1={freq1,t_min};
   int nb_echant1 = 1+(int)(freq1 * (t_max - t_min));
   double echantillons1[nb_echant1];
-  echantillonne(4,s,freq1,t_min,t_max, &ech1, nb_echant1, echantillons1);
+  echantillonne(4,s,ech1, nb_echant1, echantillons1);
   
-  struct Echantillonage ech2;
-  double freq2 = 11.0;
+  struct Echantillonage ech2={freq2,t_min};
   int nb_echant2 = 1+(int)(freq2 * (t_max - t_min));
   double echantillons2[nb_echant2];
-  echantillonne(4,s,freq2,t_min,t_max, &ech2, nb_echant2, echantillons2);
+  echantillonne(4,s,ech2, nb_echant2, echantillons2);
   
   int nb_points=30;
   double dt=(t_max-t_min)/nb_points;
   
+  printf("t         X(t)      X1(t)     X2(t)\n");
   for(double t=t_min;t<=t_max;t+=dt){
       printf("%lf8 %lf8 %lf8 %lf8\n",t,
                                  signal(t,4,s),
